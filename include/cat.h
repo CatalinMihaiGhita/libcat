c4e0430b0478754f761de05517b5dcb575a79855
Catalin Ghita <Catalin.Mihai.Ghita@gmail.com>
2018-07-20 20:59:43 +0300

    move function monad in fn.h

--- include/cat.h
+++ include/cat.h
@@ -6,6 +6,7 @@
 #include <functional>
 
 #include <monad.h>
+#include <fn.h>
 #include <all.h>
 #include <any.h>
 #include <box.h>
@@ -13,85 +14,3 @@
 #include <opt.h>
 #include <lazy.h>
 #include <vec.h>
-
-namespace cat {
-namespace kernel {
-
-template <typename T>
-struct lambda_decay;
-
-template <typename T, typename O = decltype(&T::operator())>
-struct lambda : public lambda_decay<O>
-{
-};
-
-template <typename ClassType, typename ReturnType, typename... Args>
-struct lambda_decay<ReturnType(ClassType::*)(Args...) const>
-{
-    typedef ReturnType result_type;
-};
-
-template <typename ClassType, typename ReturnType, typename... Args>
-struct lambda_decay<ReturnType(ClassType::*)(Args...)>
-{
-    typedef ReturnType result_type;
-};
-
-template <typename ReturnType, typename... Args>
-struct lambda_decay<ReturnType(*)(Args...)>
-{
-    typedef ReturnType result_type;
-};
-
-template <typename T, typename U, typename R>
-struct fmap
-{
-    template <class... Args>
-    decltype(auto) operator()(Args&&... args)
-    {
-        return u(t(std::forward<Args>(args)...));
-    }
-    T t; U u;
-};
-
-template <typename T, typename U>
-struct fmap<T, U, void>
-{
-    template <class... Args>
-    decltype(auto) operator()(Args&&... args)
-    {
-        t(std::forward<Args>(args)...);
-        return u();
-    }
-    T t; U u;
-};
-
-template <typename T, typename U>
-struct bind
-{
-    template <class... Args>
-    decltype(auto) operator()(Args&&... args)
-    {
-        return t(std::forward<Args>(args)...) >>= u;
-    }
-    T t; U u;
-};
-
-template <typename T
-         ,typename U
-         ,typename DT = typename std::decay_t<T>
-         ,typename DU = typename std::decay_t<U>
-         ,typename R = typename lambda<DT>::result_type>
-using map = typename std::conditional_t<std::is_base_of_v<monad, R>, bind<DT, DU>, fmap<DT, DU, R>>;
-
-}
-
-template <typename F
-         ,typename G
-         ,typename M = kernel::map<F,G>>
-M operator>>=(F&& f, G&& g)
-{
-    return M{ std::forward<F>(f), std::forward<G>(g) };
-}
-
-}
